a660c969eee9bbd45c68a20ab6c948b4a3b3002a
Mohd Tahzeeb Khan <78680463+Tahzeeb-web-py@users.noreply.github.com>
2022-12-19 22:27:21 +0530

    List page work done

--- employee_registration_system/Registration/views.py
+++ employee_registration_system/Registration/views.py
@@ -1,11 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import emp_forms
+from .models import Employee
 
 # Create your views here.
 def emp_list(request):
-    return render(request, "Registration/employee_list.html")
+    context={
+        'employee_list':Employee.objects.all()
+    }
+    print(context)
+    return render(request, "Registration/employee_list.html", context)
 def emp_form(request):
-    form=emp_forms()
-    return render(request, "Registration/employee_form.html", {'form':form})
+    if request.method == "GET":
+        form=emp_forms()
+        return render(request, "Registration/employee_form.html", {'form':form})
+    else:
+        form=emp_forms(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/employee/list')
+        
+
 def emp_delete(request):
     return render(request, "Registration/employee_list.html")
