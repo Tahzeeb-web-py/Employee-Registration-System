2090b07f52ce42f0bf2e12e9e458608d24c32813
Mohd Tahzeeb Khan <78680463+Tahzeeb-web-py@users.noreply.github.com>
2022-12-19 23:31:10 +0530

    Project completed!!!!

--- employee_registration_system/Registration/urls.py
+++ employee_registration_system/Registration/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.emp_form),
-    path('list/', views.emp_list)
+    path('', views.emp_form, name='employee_insert'),
+    path('<int:id>/', views.emp_form, name='employee_update'),
+    path("delete/<int:id>", views.emp_delete, name='employee_delete'),
+    path('list/', views.emp_list, name='employee_list')
 ]
--- employee_registration_system/Registration/views.py
+++ employee_registration_system/Registration/views.py
@@ -7,18 +7,28 @@ def emp_list(request):
     context={
         'employee_list':Employee.objects.all()
     }
-    print(context)
+    
     return render(request, "Registration/employee_list.html", context)
-def emp_form(request):
+def emp_form(request,id=0):
     if request.method == "GET":
-        form=emp_forms()
+        if id==0:
+            form=emp_forms()
+        else:
+            employee=Employee.objects.get(pk=id)
+            form = emp_forms(instance=employee)
         return render(request, "Registration/employee_form.html", {'form':form})
     else:
-        form=emp_forms(request.POST)
+        if id ==0:
+            form=emp_forms(request.POST)
+        else:           
+            employee=Employee.objects.get(pk=id)
+            form=emp_forms(request.POST,instance=employee)
         if form.is_valid():
             form.save()
         return redirect('/employee/list')
         
 
-def emp_delete(request):
-    return render(request, "Registration/employee_list.html")
+def emp_delete(request, id):
+    employee=Employee.objects.get(pk=id)
+    employee.delete()
+    return redirect('/employee/list')
